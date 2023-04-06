79fca87871c48db6aa0302fd8496b209803d20f7
vtewes <tewes.verena@gmail.com>
2023-04-06 11:03:30 +0200

    Add transition-timing-functions

--- 12_transition/transition.js
+++ 12_transition/transition.js
@@ -1,11 +1,29 @@
-var appearButton = document.getElementById("hideBox");
-var box = document.querySelector(".visibleBox");
+var visibilityButton = document.getElementById("toggleVisibilityButton");
+var visibleBox = document.querySelector(".visibleBox");
+var positionButton = document.getElementById("togglePositionBoxButton")
+var leftTogglePositionBox = document.querySelector(".leftTogglePositionBox")
+var timingFunctionButton = document.getElementById("timingFunctionButton");
+var easeBox = document.querySelector(".leftBoxEase");
+var easeInBox = document.querySelector(".leftBoxEaseIn");
+var linearBox = document.querySelector(".leftBoxLinear");
+var easeOutBox = document.querySelector(".leftBoxEaseOut");
+var cubicBezierBox = document.querySelector(".leftBoxCubicBezier");
 
-appearButton?.addEventListener("click", () => {
-    if (box.style.opacity == 1 || box.style.opacity == '') {
-        box.style.opacity = 0;
+visibilityButton?.addEventListener("click", () => toggleClassOnElement(visibleBox, "invisibleBox"));
+positionButton?.addEventListener("click", () => toggleClassOnElement(leftTogglePositionBox, "rightBox"));
+timingFunctionButton?.addEventListener("click", () => toggleBoxes());
+
+function toggleBoxes(){
+    toggleClassOnElement(easeBox, "rightBox");
+    toggleClassOnElement(linearBox, "rightBox");
+    toggleClassOnElement(easeInBox, "rightBox");
+    toggleClassOnElement(easeOutBox, "rightBox");
+    toggleClassOnElement(cubicBezierBox, "rightBox");
+}
+function toggleClassOnElement(element, classToToggle) {
+    if (element.classList.contains(classToToggle)) {
+        element.classList.remove(classToToggle);
     } else {
-        box.style.opacity = 1;
+        element.classList.add(classToToggle);
     }
-
-})
+}
