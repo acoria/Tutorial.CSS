424db904ca5390e219be09ca55c1ef11a8a6482b
acoria <tewes.verena@gmail.com>
2023-06-05 18:08:49 +0200

    Add Loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { LoadingSpinner } from "./snippets/loadingSpinner/LoadingSpinner";
 import { Triangle } from "./snippets/triangle/Triangle";
+import { Test } from "./test/Test";
 
 function App() {
-  return <Triangle />;
+  // return <Triangle />;
+  {
+    /* <LoadingSpinner />
+      <div style={{ backgroundColor: "blue", height: "10vh" }}></div> */
+  }
+  return <Test />;
 }
 
 export default App;
---
+++ src/test/Test.tsx
@@ -0,0 +1,6 @@
+import styles from "./Test.module.css";
+
+export const Test: React.FC = () => {
+  return <div className={styles.test}>Div</div>;
+  // return <span className={styles.test}>Span</span>;
+};
