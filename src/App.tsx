7e8410461a2ecb44347228904732df9851cd04fc
vtewes <tewes.verena@gmail.com>
2023-06-17 23:19:42 +0200

    Restructure reactTutorial components

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,25 @@ import { Paint } from "./miniApps/paint/Paint";
 import { PersonSelect } from "./personSelect/PersonSelect";
 import { PersonTurn } from "./personSelect/PersonTurn";
 import { AnimatingList } from "./reactTutorial/animatingList/AnimatingList";
-import { Component as FadingBoxComponent } from "./reactTutorial/mvce/FadingBoxComponent";
-import { TransitionComponent } from "./reactTutorial/transitions_and_animations/TransitionComponent";
+import { TransitionComponent } from "./reactTutorial/transitions_and_animations/transitionComponent/TransitionComponent";
 import { CSSTransitionComponent } from "./reactTutorial/transitions_and_animations/cssTransitionComponent/CSSTransitionComponent";
 import { CSSTransitionSidebar } from "./reactTutorial/transitions_and_animations/cssTransitionComponent/sidebar/CSSTransitionSidebar";
-import { TransitionOfSidebar } from "./reactTutorial/transitions_and_animations/transitionComponent/TransitionOfSidebar";
+import { TransitionOfSidebar } from "./reactTutorial/transitions_and_animations/transitionComponent/sidebar/TransitionOfSidebar";
 import { LoadingSpinner } from "./snippets/loadingSpinner/LoadingSpinner";
 import { MuiSlider } from "./snippets/muiSlider/MuiSlider";
 import { Triangle } from "./snippets/triangle/Triangle";
-import { Test } from "./test/Test";
 
 function App() {
-  /* <Triangle />
-      <hr></hr>
-      <hr></hr> */
+  // return<Triangle />
+  // return <Paint />;
   // return <TransitionOfSidebar />;
   // return <CSSTransitionSidebar />
-  /* <TransitionComponent /> */
-  // <AnimatingList />
-  /* <FadingBoxComponent /> */
-
-  /* <LoadingSpinner />
-      <div style={{ backgroundColor: "blue", height: "10vh" }}></div> */
-
-  // return <Test />;
+  // return <TransitionComponent />;
+  // return <AnimatingList />;
+  // return <LoadingSpinner />
   // return <PersonSelect />;
   // return <PersonTurn />;
-  // return <CSSTransitionComponent />;
-  return <Paint />;
+  return <CSSTransitionComponent />;
 }
 
 export default App;
