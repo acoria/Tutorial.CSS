791e21645cb16cdbd790cffd9407b83da3b454f5
vtewes <tewes.verena@gmail.com>
2023-07-09 20:00:59 +0200

    Add turning button to TurningPerson

--- src/miniApps/personImage/personTurn/person/TurningPerson.tsx
+++ src/miniApps/personImage/personTurn/person/TurningPerson.tsx
@@ -7,7 +7,7 @@ import { WinkingEye } from "../eye/winkingEye/WinkingEye";
 export const TurningPerson: React.FC = () => {
   const [showFront, setShowFront] = useState(true);
   return (
-    <div className={styles.personTurn}>
+    <div className={styles.turningPerson}>
       <img
         className={`${styles.image} ${showFront ? "" : styles.flipSideways}`}
         src={person}
@@ -16,9 +16,12 @@ export const TurningPerson: React.FC = () => {
       <Buttock show={!showFront} />
       <WinkingEye show={showFront} positionInPercentFromLeft={50} />
       <WinkingEye show={showFront} positionInPercentFromLeft={60} />
-      <button onClick={() => setShowFront((previous) => !previous)}>
-        {showFront ? "Flip to back" : "Flip to front"}
-      </button>
+      <span
+        className={`material-symbols-outlined ${styles.turnButton}`}
+        onClick={() => setShowFront((previous) => !previous)}
+      >
+        refresh
+      </span>
     </div>
   );
 };
