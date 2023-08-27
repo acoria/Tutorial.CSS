b891dbb68707e09f4736abdfdf83f454bf43a961
acoria <tewes.verena@gmail.com>
2023-08-27 15:12:09 +0200

    Adjust tile amount size

--- src/test/pizzaApp/ingredientList/IngredientList.tsx
+++ src/test/pizzaApp/ingredientList/IngredientList.tsx
@@ -1,10 +1,20 @@
 import { Ingredient } from "../ingredient/Ingredient";
 import styles from "./IngredientList.module.css";
-import flourImage from "../assets/flourBrown.png";
-// import saltImage from "../assets/saltBlue.png";
+// import flourImage from "../assets/flourBrown.png";
+import { useEffect, useState } from "react";
 
 export const IngredientList: React.FC = () => {
-  const flour = <Ingredient name="Flour" amount="1600g" image={flourImage} />;
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    const loadImage = async () => {
+      const response = await import("../assets/flourBrown.png");
+      setImage(response.default);
+    };
+    loadImage();
+  }, []);
+
+  const flour = <Ingredient name="Flour" amount="1600g" image={image} />;
 
   return (
     <div className={styles.ingredientList}>
