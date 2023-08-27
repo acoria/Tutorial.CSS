6e718a15e7baabd13ec74ab6593931497b0a16a8
acoria <tewes.verena@gmail.com>
2023-08-27 16:00:21 +0200

    Add salt and water to PizzaApp

--- src/test/pizzaApp/ingredientList/IngredientList.tsx
+++ src/test/pizzaApp/ingredientList/IngredientList.tsx
@@ -1,26 +1,19 @@
 import { Ingredient } from "../ingredient/Ingredient";
 import styles from "./IngredientList.module.css";
-// import flourImage from "../assets/flourBrown.png";
-import { useEffect, useState } from "react";
+import flourImage from "../assets/wheat_brown.png";
+import saltImage from "../assets/salt_brown.png";
+import waterImage from "../assets/water_brown.png";
 
 export const IngredientList: React.FC = () => {
-  const [image, setImage] = useState("");
-
-  useEffect(() => {
-    const loadImage = async () => {
-      const response = await import("../assets/flourBrown.png");
-      setImage(response.default);
-    };
-    loadImage();
-  }, []);
-
-  const flour = <Ingredient name="Flour" amount="1600g" image={image} />;
+  const flour = <Ingredient name="Flour" amount="1600g" image={flourImage} />;
+  const salt = <Ingredient name="Salt" amount="63g" image={saltImage} />;
+  const water = <Ingredient name="Water" amount="250ml" image={waterImage} />;
 
   return (
     <div className={styles.ingredientList}>
       {flour}
-      {flour}
-      {flour}
+      {salt}
+      {water}
     </div>
   );
 };
