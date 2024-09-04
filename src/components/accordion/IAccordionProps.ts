15c6c9732a670cd96bf9b1b2de038c62a794d4a6
acoria <tewes.verena@gmail.com>
2024-09-04 09:32:49 +0200

    Add classNames to Accordion

--- src/components/accordion/IAccordionProps.ts
+++ src/components/accordion/IAccordionProps.ts
@@ -3,4 +3,7 @@ import { ReactElement } from "react";
 export interface IAccordionProps {
   titles: string[] | ReactElement[];
   children: ReactElement | ReactElement[];
+  headerClassName?: string;
+  titleClassName?: string;
+  contentClassName?: string;
 }
